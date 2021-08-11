aa1d6a93e3cb3d78cb2db49bcabc7a1250ec9c64
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-08-10 23:50:50 -0300

    create user

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import {Router} from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json([message: 'hello world']);
+routes.get('/', async (req, res) => {
+
+  const user = await User.create({
+    name: 'Tundê Cavalcante',
+    email: 'ayo@hotmail.com',
+    password_hash: '1232342342'
+  });
+
+  return res.json(user);
 })
 
 export default routes;
