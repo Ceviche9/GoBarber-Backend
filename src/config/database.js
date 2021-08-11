f503f89ed734c5efe0f7949f539f93ff44647ede
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-08-10 23:25:41 -0300

    db name fix

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-  dialect: 'postegres',
+  dialect: 'postgres',
   host: 'localhost',
-  usernasme: 'postgres',
+  username: 'postgres',
   password: 'docker',
   database: 'Gobarber',
   define: {
